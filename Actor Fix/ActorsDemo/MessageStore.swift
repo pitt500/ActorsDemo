5942db6d6bf7a85c9c70559396003cead270298b
pitt500 <>
2021-07-23 07:56:34 -0700

    Add counter of messages

--- Actor Fix/ActorsDemo/MessageStore.swift
+++ Actor Fix/ActorsDemo/MessageStore.swift
@@ -19,6 +19,7 @@ actor MessageStore {
         //Change this code to only 1 and will work as expected
         let randomNumberOfMessages = Int.random(in: 1...10)
 
+        print("Receiving \(randomNumberOfMessages) messages")
         DispatchQueue.concurrentPerform(iterations: randomNumberOfMessages) { iteration in
             let message = Message(content: "Message #\(iteration)")
 
